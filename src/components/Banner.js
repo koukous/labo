841a0e8be7ed22127467f865ca1eebfc43e75b3b
koukous <hamane.el.mehdi@gmail.com>
2021-07-02 00:11:11 +0100

    add logo and style to panier

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,14 @@
 import '../styles/Banner.css';
+import logo from '../assets/logo.png';
 
 function Banner () {
-
+const title = 'La maison jungle';
     return (
         <div className='lmj-banner'>
-            <h1>La maison jungle</h1>
+            <img src={logo} alt = {title} className = 'lmj-logo'/>
+            <h1>{title}</h1>
         </div>
         )
 }
-dsdsd
+
 export default Banner
